c2a7bf6a38cdcc9b20f26b1e7537a5cf2b088ea8
Henrique Amorim <henrique.amorim.ferraz@gmail.com>
2022-09-12 19:25:32 -0300

    Criando Models.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'color',
+        'user_id'
+    ];
+
+    public function user() {
+        $this->belongsTo(User::class);
+    }
+    public function categories() {
+        $this->hasMany(Task::class);
+    }
 }
