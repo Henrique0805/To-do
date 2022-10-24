1962ec5bacffc2b9efab70c5679ec3e4bed4db0d
Henrique Amorim <henrique.amorim.ferraz@gmail.com>
2022-10-24 19:16:47 -0300

    Criando componentização dos layouts

---
+++ resources/views/login.blade.php
@@ -0,0 +1,8 @@
+<x-layout page="B7Web Todo: Login">
+    <x-slot:btn>
+        <a href="#" class="btn btn-primary">
+            Registre-se
+        </a>
+    </x-slot:btn>
+    Tela de login
+</x-layout>
