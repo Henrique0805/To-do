4b33f5fb744692cc1a4e5e1b32ce1f3afd95fd4e
Henrique Amorim <87548301+Henrique0805@users.noreply.github.com>
2022-11-03 19:26:23 -0300

    To do 6 (#6)
    * Criando componentização dos layouts
    
    * Criando componentes de tasks.
    
    * Criando navegação de páginas

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+<x-layout page="B7Web Todo: Login">
+    <x-slot:btn>
+        <a href="{{route('register')}}" class="btn btn-primary">
+            Registre-se
+        </a>
+    </x-slot:btn>
+    Tela de login
+    <a href="{{route('home')}}">
+        Ir para home
+    </a>
+</x-layout>
