72448f53a733cdf98d0f241d2d997a1525c7bd6e
Henrique Amorim <henrique.amorim.ferraz@gmail.com>
2022-11-03 19:24:29 -0300

    Criando navegação de páginas

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,11 @@
 <x-layout page="B7Web Todo: Login">
     <x-slot:btn>
-        <a href="#" class="btn btn-primary">
+        <a href="{{route('register')}}" class="btn btn-primary">
             Registre-se
         </a>
     </x-slot:btn>
     Tela de login
+    <a href="{{route('home')}}">
+        Ir para home
+    </a>
 </x-layout>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,8 @@
+<x-layout page="B7Web Todo: Login">
+    <x-slot:btn>
+        <a href="{{route('login')}}" class="btn btn-primary">
+            Já possui conta? Faça login
+        </a>
+    </x-slot:btn>
+    Tela de Registro
+</x-layout>
