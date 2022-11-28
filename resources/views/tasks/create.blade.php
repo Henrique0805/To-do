fe0686038edfa28ebf4248c0ff4859d981d4ac61
Henrique Amorim <87548301+Henrique0805@users.noreply.github.com>
2022-11-28 19:40:07 -0300

    To do 7 (#7)
    * Corrigindo erro de task. E ajustando componentes
    
    * Criando inclusão de tasks.

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,5 +4,35 @@
             Voltar
         </a>
     </x-slot:btn>
-    Tela de criar uma tarefa
+    <section id="create_task_section">
+        <h1>Criar Tarefa</h1>
+        <form>
+            <div class="inputArea">
+                <label for="title">
+                    Título da Task
+                </label>
+                <input id="title" name="title" placeholder="Digite o título da tarefa" required />
+            </div>
+            <div class="inputArea">
+                <label for="due_date">
+                    Data de Realização
+                </label>
+                <input type="date" id="due_date" name="due_date" placeholder="Digite o título da tarefa" required />
+            </div>
+            <div class="inputArea">
+                <label for="category">
+                    Categoria
+                </label>
+                <select id="category" name="category" required>
+                    <option selected disabled value="">Selecione a categoria</option>
+                </select>
+            </div>
+            <div class="inputArea">
+                <label for="description">
+                    Descrição de tarefa
+                </label>
+                <textarea id="description" name="description" placeholder="Digite uma descrição para sua tarefa" ></textarea>
+            </div>
+        </form>
+    </section>
 </x-layout>
