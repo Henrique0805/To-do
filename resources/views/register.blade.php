68a50b94f73f8eb5dea7fc091ce788cb835f34a6
henrique.amorim <henrique.amorim@zanthus.com.br>
2023-01-25 17:04:12 -0300

    Montando tela de registro de usuários.

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,8 +1,31 @@
 <x-layout page="B7Web Todo: Login">
     <x-slot:btn>
-        <a href="{{route('login')}}" class="btn btn-primary">
-            Já possui conta? Faça login
+        <a href="{{route('home')}}" class="btn btn-primary">
+            Voltar
         </a>
     </x-slot:btn>
-    Tela de Registro
+    <section id="task_section">
+        <h1>Registre-se</h1>
+
+        @if ($errors->any())
+            <ul class="alert alert-erro">
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        @endif
+
+        <form method="POST" action="{{route('user.register_action')}}">
+            @csrf
+            <x-form.text_input name="name" label="Seu nome" placeholder="Seu nome"
+            required="required"/>
+            <x-form.text_input type="email" name="email" label="Seu email"
+            placeholder="Seu email" required="required"/>
+            <x-form.text_input type="password" name="password" label="Sua senha"
+            placeholder="Sua senha" required="required"/>
+            <x-form.text_input type="password" name="password_confirmation" label="Confirme sua senha"
+            placeholder="Confirme sua senha" required="required"/>
+            <x-form.form_button resetTxt="Limpar" submitTxt="Registrar-se"></x-form.form_button>
+        </form>
+    </section>
 </x-layout>
