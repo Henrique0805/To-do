bac1a05f92701e2309d8139d8173c5b117e34db1
Henrique Amorim <87548301+Henrique0805@users.noreply.github.com>
2023-01-27 19:43:00 -0300

    To do 11 (#13)
    * Criando tela de login e proteção de senha.
    
    * Criando autenticação.
    
    ---------
    
    Co-authored-by: henrique.amorim <henrique.amorim@zanthus.com.br>

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,13 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function index(Request $request) {
+        if(Auth::check()) {
+            return redirect()->route('home');
+        }
         return view('login');
     }
+    public function login_action(Request $request) {
+        $validator = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+        if(Auth::attempt($validator)){
+            return redirect(route('home'));
+        }
+    }
     public function register(Request $request) {
+        if(Auth::check()) {
+            return redirect()->route('home');
+        }
         return view('register');
     }
     public function register_action(Request $request) {
@@ -22,9 +39,15 @@ class AuthController extends Controller
         ]);
 
         $data = $request->only('name','email','password');
+        $data['password'] = Hash::make($data['password']);
 
         User::create($data);
 
         return redirect(route('login'));
+    }
+
+    public function logout() {
+        Auth::logout();
+        return redirect()->route('login');
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,21 @@
 <x-layout page="B7Web Todo: Login">
-    <x-slot:btn>
-        <a href="{{route('register')}}" class="btn btn-primary">
-            Registre-se
-        </a>
-    </x-slot:btn>
-    Tela de login
-    <a href="{{route('home')}}">
-        Ir para home
-    </a>
+    <section id="task_section">
+        <h1>Autenticação</h1>
+        @if ($errors->any())
+            <ul class="alert alert-erro">
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        @endif
+
+        <form method="POST" action="{{route('user.login_action')}}">
+            @csrf
+            <x-form.text_input type="email" name="email" label="Seu email"
+            placeholder="Seu email" required="required"/>
+            <x-form.text_input type="password" name="password" label="Sua senha"
+            placeholder="Sua senha" required="required"/>
+            <x-form.form_button resetTxt="Limpar" submitTxt="Login"></x-form.form_button>
+        </form>
+    </section>
 </x-layout>
