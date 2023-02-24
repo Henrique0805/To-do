7aff03a596fa89fe7861024f4a989fcbd8a638f9
henrique.amorim <henrique.amorim@zanthus.com.br>
2023-02-24 19:29:34 -0300

    Configurando datas e filtros de datas.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index(Request $request) {
 
-        $tasks = Task::all()->take(4);
+        if($request->date) {
+            $filterDate = $request->date;
+        }else{
+            $filterDate = date('Y-m-d');
+        }
 
-        return view('home', ['tasks' => $tasks]);
+        $carbonDate = Carbon::createFromDate($filterDate);
+        Carbon::setLocale('pt_BR');
+
+        $data['date_as_string'] = $carbonDate->translatedFormat('d').' de '. ucfirst($carbonDate->translatedFormat('M'));
+        $data['date_prev_button'] = $carbonDate->addDay(-1)->format('Y-m-d');
+        $data['date_next_button'] = $carbonDate->addDay(2)->format('Y-m-d');
+
+        $data['tasks'] = Task::whereDate('due_date', $filterDate)->get();
+
+        return view('home', $data);
     }
 }
